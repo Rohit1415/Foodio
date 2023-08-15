452abc992712501b4007c4d6702196f642840f99
Rohit1415 <bhaturohit549@gmail.com>
2023-08-15 18:00:56 +0530

    first commit

---
+++ src/Routes.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "pages/NotFound";
+const Homepage = React.lazy(() => import("pages/Homepage"));
+const Checkout = React.lazy(() => import("pages/Checkout"));
+const Orderonline = React.lazy(() => import("pages/Orderonline"));
+const Contact = React.lazy(() => import("pages/Contact"));
+const Reservation = React.lazy(() => import("pages/Reservation"));
+const Aboutus = React.lazy(() => import("pages/Aboutus"));
+const Menu = React.lazy(() => import("pages/Menu"));
+const ProjectRoutes = () => {
+  return (
+    <React.Suspense fallback={<>Loading...</>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/orderonline" element={<Orderonline />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </React.Suspense>
+  );
+};
+export default ProjectRoutes;
